fa695b3f9be48e85b8c68192445027de7f0e984c
ronedr <ronedr@post.bgu.ac.il>
2025-07-25 18:51:42 +0300

    update notbooks and utils function

--- utils/problem_utils.py
+++ utils/problem_utils.py
@@ -1,10 +1,12 @@
+import brax.envs as brax_envs
 import gymnax
 from evosax.problems import Problem
 from evosax.problems.bbob.bbob import BBOBProblem
+from evosax.problems.networks import categorical_output_fn, tanh_output_fn
 from evosax.problems.rl.brax import BraxProblem
 from evosax.problems.rl.gymnax import GymnaxProblem
 from evosax.problems.vision.torchvision import TorchVisionProblem
-import brax.envs as brax_envs
+from gymnax.environments.spaces import Discrete, Box
 
 
 def get_problem_name(problem: Problem):
@@ -20,15 +22,30 @@ def get_problem_name(problem: Problem):
     return f"{problem.__class__.__name__}/{problem_name}"
 
 
-def get_problem_action_size(env_name: str):
+def get_problem_settings(env_name: str):
+    """
+    Returns an appropriate output function and chosen output dimension
+    based on the action space of the given environment name.
+    Supports both discrete (gymnax) and continuous (Box) action spaces.
+    """
+    # Brax environments (via gymnax wrapper)
     if env_name in list(brax_envs._envs.keys()):
-        return brax_envs._envs[env_name]().action_size
+        env = brax_envs._envs[env_name]()
+        num_actions = env.action_size
+        output_fn = tanh_output_fn  # continuous control in Brax
+        return num_actions, output_fn
+    # Gymnax environments
     elif env_name in gymnax.registered_envs:
         env, env_params = gymnax.make(env_name)
         action_space = env.action_space(env_params)
-        if hasattr(action_space, "n"):
-            return action_space.n
+        if isinstance(action_space, Discrete):
+            num_actions = action_space.n
+            output_fn = categorical_output_fn
+        elif isinstance(action_space, Box):
+            num_actions = int(action_space.shape[0])
+            output_fn = tanh_output_fn
         else:
-            return int(action_space.shape[0])
+            raise ValueError(f"Unsupported action space type: {action_space}")
+        return num_actions, output_fn
     else:
-        raise Exception("No found problem name!!")
+        raise ValueError(f"Unrecognized environment name: '{env_name}'")
