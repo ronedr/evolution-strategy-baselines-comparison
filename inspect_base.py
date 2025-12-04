17d072cd75593e4364978e650e22f82010d6bbb9
eliadsbgu <37577990+eliadsbgu@users.noreply.github.com>
2025-12-04 14:19:45 +0200

    evotf runner

---
+++ inspect_base.py
@@ -0,0 +1,5 @@
+import inspect
+import evosax.algorithms.distribution_based.base
+
+print("DistributionBasedAlgorithm.tell signature:")
+print(inspect.signature(evosax.algorithms.distribution_based.base.DistributionBasedAlgorithm.tell))
